16ce37e21b16325eaa45e009f19f0c2d5765cda9
sohagmia360 <gdsohag360@gmail.com>
2022-03-06 08:09:25 +0600

    fix some Error

--- ServerPostMethos.js
+++ ServerPostMethos.js
@@ -6,18 +6,21 @@ const server = http.createServer((req, res) => {
     res.write(`
         <body>
         <form method="post" action="/prosess">
-            <input type="text">
+            <input type="text" name="massage">
         </form>
         </body>
         `)
         console.log('res')
     res.end();
   }else if(req.url === '/prosess' && req.method === 'POST'){
-    req.on('data', (chunk)=>{
-        console.log(chunk)
+    req.on('data', (chunk) =>{
+        console.log(chunk.toString())
     })
     res.write('succesFully Submited')
     res.end()
-  } 
+  } else{
+    res.write('not found')
+    res.end()
+  }
 });
 server.listen(3000)
